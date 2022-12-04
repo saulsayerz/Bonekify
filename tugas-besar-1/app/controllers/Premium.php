f25c700dc09807df6a40f67249ae2398b0bc05af
Saul Sayers <saulsayers@gmail.com>
2022-12-04 13:50:05 +0700

    Finalisasi merapihkan
    Co-Authored-By: Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
    Co-Authored-By: Rizky Ramadhana Putra Kusnaryanto <65774061+rizkyramadhana26@users.noreply.github.com>

--- tugas-besar-1/app/controllers/Premium.php
+++ tugas-besar-1/app/controllers/Premium.php
@@ -5,6 +5,15 @@ class Premium extends Controller{
     $data['title'] = "Daftar Penyanyi Premium | Bonekify";
     $data['route'] = 'premium';
     $data['status'] = $this->model("subscription_model")->getStatus($_SESSION['user_id']);
+    foreach($data['status'] as $id => $status){
+      if($status=='PENDING'){
+        $acc = $this->model("soap_model")->validate($id,$_SESSION['user_id']);
+        if($acc){
+          $data['status'][$id] = 'ACCEPTED';
+          $this->model("subscription_model")->setStatus($id,$_SESSION['user_id'],"ACCEPTED");
+        }
+      }
+    }
     $this->view('Templates/header',$data);
     $this->view("Premium/index",$data);
     $this->view('Templates/footer',$data);
--- tugas-besar-1/app/models/soap_model.php
+++ tugas-besar-1/app/models/soap_model.php
@@ -41,5 +41,47 @@ class soap_model{
     
     //echo $input_celsius . ' Celsius => ' . $data . ' Fahrenheit';
   }
+
+  public static function validate($creator_id,$subscriber_id){
+    $webservice_url = "http://bonekify-soap-service:1401/";
+    
+    $request_param = '<?xml version="1.0" encoding="utf-8"?>
+    <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+        <Body>
+            <validate xmlns="http://services.binotifysoap/">
+                <arg0 xmlns="">'.$creator_id.'</arg0>
+                <arg1 xmlns="">'.$subscriber_id.'</arg1>
+            </validate>
+        </Body>
+    </Envelope>';
+
+    $headers = array(
+        'Content-Type: text/xml; charset=utf-8',
+        'Content-Length: '.strlen($request_param),
+        'x-api-key:321321',
+        'origin:PLAIN'
+    );
+    
+    $ch = curl_init($webservice_url);
+    curl_setopt ($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt ($ch, CURLOPT_POSTFIELDS, $request_param);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    
+    $data = curl_exec ($ch);
+    
+    // $result = $data;
+    
+    // if ($result === FALSE) {
+    //     printf("CURL error (#%d): %s<br>\n", curl_errno($ch),
+    //     htmlspecialchars(curl_error($ch)));
+    // }
+    
+    curl_close ($ch);
+    return $data;
+    //echo $input_celsius . ' Celsius => ' . $data . ' Fahrenheit';
+  }
 }
 ?>
